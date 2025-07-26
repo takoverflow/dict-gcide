7cc8733f5bed3ad0561281157724bae207991b6a
S0AndS0 <strangerthanbland@gmail.com>
2025-07-26 14:22:03 -0700

    :fire: Merge `gcide` unpacking into default...
    This also brings in most changes that _should_ be compatible while
    related PR is reviewed by NixOS team

--- default.nix
+++ default.nix
@@ -1,66 +1,62 @@
 {
-  version ? "0.53",
-  pkgs ? import <nixpkgs> { },
+  autoreconfHook,
+  bison,
+  dict,
+  fetchurl,
+  flex,
+  lib,
+  # libmaa,
+  stdenv,
   ...
 }:
 
 let
-  ## Sed scripts are versioned to discourage forgetting to yoink updates from AUR
+  ## TODO: make this not ugly?
+  libmaa = builtins.head (builtins.filter (x: x.pname == "libmaa") dict.buildInputs);
+
   sed-scripts = {
     check = ./sed-scripts/check.sed;
     fixes = ./sed-scripts/fixes.sed;
     post_webfilter = ./sed-scripts/post_webfilter.sed;
   };
 
-  ## TODO: make this not ugly?
-  libmaa = builtins.head (builtins.filter (x: x.pname == "libmaa") pkgs.dict.buildInputs);
-
-  gcide = pkgs.callPackage ./gcide.nix { inherit version pkgs; };
-
   _major_debver = "0.48";
   _debver = "${_major_debver}.5+nmu4";
 in
-pkgs.stdenv.mkDerivation rec {
-  inherit version;
+stdenv.mkDerivation rec {
+  version = "0.53";
 
   pname = "dict-gcide";
   name = pname;
   dbName = pname;
-  locale = "en_US.UTF-8";
 
-  src = pkgs.fetchurl {
-    url = "https://deb.debian.org/debian/pool/main/d/${pname}/${pname}_${_debver}.tar.xz";
-    hash = "sha256-45ITB4SnyRycjchdseFP5J+XhZfp6J2Dm5q+DJ/N4A4=";
-  };
+  sourceRoot = "./${pname}";
+  srcs = [
+    (fetchurl {
+      url = "https://deb.debian.org/debian/pool/main/d/${pname}/${pname}_${_debver}.tar.xz";
+      hash = "sha256-45ITB4SnyRycjchdseFP5J+XhZfp6J2Dm5q+DJ/N4A4=";
+    })
+    (fetchurl {
+      url = "https://ftp.gnu.org/gnu/gcide/gcide-${version}.tar.xz";
+      hash = "sha256-06y9mE3hzgItXROZA4h7NiMuQ24w7KLLD7HAWN1/MZ8=";
+    })
+  ];
 
-  buildInputs =
-    with pkgs;
-    [
-      ## Needed for: autoreconf
-      autoconf
-      automake
-      ## Needed for: make
-      bison
-      flex
-      ## Needed for: dictzip
-      dict
-    ]
-    ++ [
-      gcide
-      libmaa
-    ];
+  buildInputs = [
+    libmaa
+  ];
 
-  patchPhase = ''
+  nativeBuildInputs = [
+    autoreconfHook
+    bison
+    dict
+    flex
+  ];
+
+  prePatch = ''
     sed -Ei "/The Collaborative International Dictionary of English v.${_major_debver}/ {
       s/${_major_debver}/${version}/ ;
     }" scan.l;
-
-    rm config.guess config.h.in config.sub configure install-sh;
-  '';
-
-  configurePhase = ''
-    autoreconf -fis;
-    ./configure;
   '';
 
   enableParallelBuilding = false;
@@ -70,7 +66,7 @@ pkgs.stdenv.mkDerivation rec {
     ## Do the conversion explicitly, instead of `make db', to account for all
     ## the differences to the original build process.
     ## LANG=C is required so that the index file is properly sorted.
-    sed -Ef "${sed-scripts.fixes}" "${gcide.out}"/CIDE.?  |
+    sed -Ef "${sed-scripts.fixes}" ../gcide-${version}/CIDE.?  |
       sed -f debian/sedfile |
       ./webfilter |
       sed -Ef "${sed-scripts.post_webfilter}" |
@@ -94,28 +90,20 @@ pkgs.stdenv.mkDerivation rec {
   '';
 
   installPhase = ''
-    install -m 0755 -d "$out/share/dictd";
-    install -m 0644 -t "$out/share/dictd/" ./gcide.{dict.dz,index};
+    install -Dm 0644 -t "$out/share/dictd/" ./gcide.{dict.dz,index};
 
-    echo "${locale}" > "$out/share/dictd/locale";
+    echo "en_US.UTF-8" > "$out/share/dictd/locale";
 
-    install -m 0755 -d "$out/share/doc/dict-gcide";
-    install -m 0644 -t "$out/share/doc/dict-gcide/" "${gcide.out}"/{README,INFO,pronunc.txt};
+    install -Dm 0644 -t "$out/share/doc/dict-gcide/" ../gcide-${version}/{README,INFO,pronunc.txt};
   '';
 
   meta = {
-    description = "GNU version of the Collaborative International Dictionary of English for dictd et al";
-    homepage = "https://deb.debian.org/debian/pool/main/d/${pname}";
-    license = pkgs.lib.licenses.gpl3Plus;
-    maintainers = [
-      {
-        email = "S0AndS0@digital-mercenaries.com";
-        github = "S0AndS0";
-        githubId = 4116150;
-        name = "S0AndS0";
-      }
+    description = "GNU version of the Collaborative International Dictionary of English";
+    homepage = "https://gcide.gnu.org.ua/";
+    license = lib.licenses.gpl3Plus;
+    maintainers = with lib.maintainers; [
+      S0AndS0
     ];
-    platforms = pkgs.lib.platforms.unix;
+    platforms = lib.platforms.unix;
   };
 }
-# vim: expandtab shiftwidth=2 tabstop=2
