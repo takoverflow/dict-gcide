feccb582e40eb22492729f578fef3e510c526534
Prashant Tak <prashant.tak@sap.com>
2025-10-15 08:41:55 +0530

    Update flake.nix to expose dict-gcide as a package

--- flake.nix
+++ flake.nix
@@ -9,5 +9,20 @@
       formatter = forAllSystems (system: 
         nixpkgs.legacyPackages.${system}.nixfmt-tree
       );
+
+      packages = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { 
+            inherit system;
+            config.allowUnfree = true;
+          };
+        in {
+          default = pkgs.callPackage ./default.nix { };
+        }
+      );
+      
+      overlays.default = final: prev: {
+        dict-gcide = final.callPackage ./default.nix { };
+      };
     };
 }
