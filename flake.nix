c54ef56ad80c39608871d408c2fe27ae734dd829
Prashant Tak <prashant.tak@sap.com>
2025-10-15 08:26:42 +0530

    Update flake.nix with additional systems

--- flake.nix
+++ flake.nix
@@ -2,9 +2,12 @@
   outputs =
     { nixpkgs, self }@inputs:
     let
-      system = "x86_64-linux";
+      systems = [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
     in
     {
-      formatter.${system} = nixpkgs.legacyPackages.${system}.nixfmt-tree;
+      formatter = forAllSystems (system: 
+        nixpkgs.legacyPackages.${system}.nixfmt-tree
+      );
     };
 }
